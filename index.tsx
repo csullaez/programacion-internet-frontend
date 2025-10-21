2098b295ff99343fc36a511cebc22f3e4e47afe2
Carlos Sullaez <jsullaez@att.gob.bo>
2025-10-20 21:36:23 -0400

    feat: :rocket: Adicion de tailwind en proyecto

--- index.tsx
+++ index.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
-
-// estilos base (deja comentado el SCSS por ahora)
-// import './style.scss'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+import "./tailwind.css"
+import App from "./App"
 
 createRoot(document.getElementById('root')!).render(<App />)
